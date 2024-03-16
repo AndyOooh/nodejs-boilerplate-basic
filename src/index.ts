a455d55acded0cc40c9242f4e1ca95c13236ee32
AndyOoh <andy.oeee@gmail.com>
2024-03-16 19:35:04 +0700

    remove nodemon, ts-node

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { testFunction } from './helpers.js';
 
 const run = async () => {
   const todo = await testFunction();
-  console.log('Todo 2: ', todo);
+  console.log('Todo #1: ', todo);
 };
 
 run();
