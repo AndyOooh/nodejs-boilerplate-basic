7eec9de23d943b5c79a0ddc5bafb564cde96e8b4
AndyOoh <andy.oeee@gmail.com>
2024-03-16 19:31:40 +0700

    add/modify scripts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const testFunction = async () => {
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
   return data;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { testFunction } from './helpers.js';
 
 const run = async () => {
-  const res = await testFunction();
-  console.log(res);
+  const todo = await testFunction();
+  console.log('Todo 2: ', todo);
 };
 
 run();
